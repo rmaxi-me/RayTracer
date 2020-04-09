771b3257aa86bc381a0f136afe3a737db2e806ef
Maxime Houis <maxime.houis@epitech.eu>
2020-04-09 13:36:06 +0200

    Finish argument parser, imporve CI

--- Engine/src/ArgumentParser.cpp
+++ Engine/src/ArgumentParser.cpp
@@ -7,9 +7,48 @@
 
 #include "ArgumentParser.hpp"
 
-ArgumentParser::ArgumentParser(int ac, char **av)
-        : m_argc{ac}, m_argv{av}
+bool APFuncs::boolean(const std::string &raw)
 {
+    return raw == "1";
+}
+
+float APFuncs::floating(const std::string &raw)
+{
+    return std::stof(raw);
+}
+
+double APFuncs::double_float(const std::string &raw)
+{
+    return std::stod(raw);
+}
+
+ArgumentParser::ArgumentParser(int ac, const char *const *av, std::string_view stopParsing)
+        : m_argc{ac}, m_argv{av}, m_binName{av[0]}, m_stopParsing{stopParsing}
+{
+    for (int i = 1; i < ac; i++) {
+        auto flag = std::string_view(av[i]);
+
+        if (flag == m_stopParsing)
+            break;
+
+        if (flag.length() >= 2) {
+            if (flag[0] == '-') {
+                if (flag[1] == '-') {
+                    flag.remove_prefix(2);
+                } else {
+                    flag.remove_prefix(1);
+                }
+
+                if (i + 1 < ac && std::string_view(av[i + 1])[0] != '-') {
+                    m_args.insert(std::make_pair(flag, std::string(av[i + 1])));
+                } else {
+                    m_args.insert(std::make_pair(flag, std::string("1")));
+                }
+            } else {
+                m_args.insert(std::make_pair(flag, std::string("1")));
+            }
+        }
+    }
 }
 
 int ArgumentParser::getArgc() const noexcept
@@ -20,4 +59,9 @@ int ArgumentParser::getArgc() const noexcept
 const char *const *ArgumentParser::getArgv() const noexcept
 {
     return m_argv;
+}
+
+std::string_view ArgumentParser::getBinName() const noexcept
+{
+    return m_binName;
 }
--- Tests/src/TestsArgumentParser.cpp
+++ Tests/src/TestsArgumentParser.cpp
@@ -6,11 +6,27 @@
 */
 
 #include <criterion/criterion.h>
+#include <iostream>
 
 #include "Engine/ArgumentParser.hpp"
 
-Test(ArgumentParser, ctor)
+Test(ArgumentParser, Constructors)
 {
-    auto arg = ArgumentParser{0, nullptr};
-    (void) arg;
+    const char *argv[] = {"bin", "--fullscreen", "--width", "1600", "--height", "900"};
+    const auto parser = ArgumentParser{6, argv};
+
+    auto fullscreen = parser.getOrDefault<bool>("fullscreen", "f", false, APFuncs::boolean);
+    auto missing = parser.getOrDefault<bool>("missing", "m", false, APFuncs::boolean);
+    auto width = parser.getOrDefault<int>("width", "w", 1920, APFuncs::integer<int>);
+    auto height = parser.getOrDefault<int>("height", "h", 1080, APFuncs::integer<int>);
+    auto depth = parser.getOrDefault<int>("depth", "d", 32, APFuncs::integer<int>);
+
+    cr_assert(parser.getBinName() == "bin");
+    cr_assert(parser.getArgc() == 6);
+    cr_assert(parser.getArgv() == argv);
+    cr_assert(fullscreen);
+    cr_assert(!missing);
+    cr_assert(width == 1600);
+    cr_assert(height == 900);
+    cr_assert(depth == 32);
 }
