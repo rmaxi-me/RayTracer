0ad9d51935e960f67824c5f380ec84f4250ed15f
Pierre Langlois <pierrelgs.pro@hotmail.com>
2020-04-12 16:30:18 +0200

    [ADD] Draw a red Sphere

--- RayTracer/src/test.cpp
+++ RayTracer/src/test.cpp
@@ -3,8 +3,26 @@
 #include "Vector/Vector3.hpp"
 #include "Ray/Ray.hpp"
 
+int hitSphere(const raymath::Vector3& center, float Radius, const raylib::Ray &r)
+{
+    // equation is : t^2 dot(B,B) + 2t dot(B,A-C) + dot(A-C, A-C)-R^2
+    // form of ax^2 + bx + c = 0
+    // where a = dot(B,B)
+    //       b = 2dot(B, A-C)
+    //       c = dot(A-C, A-C)-R^2
+    raymath::Vector3 oc = r.getOrigin() - center;
+    float a = raymath::dotProduct(r.getDirection(), r.getDirection());
+    float b = 2.0f * raymath::dotProduct(oc, r.getDirection());
+    float c = raymath::dotProduct(oc, oc) - Radius*Radius;
+    float discriminant = b*b - 4*a*c;
+    return discriminant > 0;
+}
+
 raymath::Vector3 linearInterpolation(raylib::Ray ray)
 {
+    if (hitSphere(raymath::Vector3(0,0,-1), 0.5f, ray))
+        return raymath::Vector3(1,0,0);
+
     raymath::Vector3 vecteurUnitaire = normalize(ray.getDirection());
     
     //get a factor depending of the y direction of the ray (up/downess)
@@ -13,7 +31,7 @@ raymath::Vector3 linearInterpolation(raylib::Ray ray)
 
     //formula of the linear interpolarion got a gradient of color
     return (1.0f-t) * raymath::Vector3(0.0,1.0,1.0) + t*raymath::Vector3(1.0, 0.0, 1.0);
-                            //blue green                        //purple
+                                //blue green                        //purple
 }
 
 int main(int argc, char const *argv[])
@@ -21,13 +39,13 @@ int main(int argc, char const *argv[])
     int nx = 800;
     int ny = 600;
 
-    raylib::Window window(800, 600, "Test");
+    raylib::Window window(nx, ny, "Test");
 
     window.changeClearColor(BLACK);
 
     raymath::Vector3 l(-2,-1,-1);
     raymath::Vector3 h(4,0,0);
-    raymath::Vector3 v(0,2,0);
+    raymath::Vector3 v(0,3,0);
     raymath::Vector3 o(0,0,0);
 
     while(window.isOpen())
@@ -42,9 +60,8 @@ int main(int argc, char const *argv[])
 
                 //Projection of the ray depending of the size of the screen
                 raylib::Ray ray(o, l+Vu*h+Vv*v);
+
                 raymath::Vector3 col = linearInterpolation(ray);
-                // raymath::Vector3 col(colo.x() * 255.99, colo.y() * 255.99, colo.z() * 255.99);
-                // std::cout << col << std::endl;
                 DrawPixel(i, j, Color{(int)(col.x() * 255.99), (int)(col.y() * 255.99), (int)(col.z() * 255.99), 255});
             }
         }
