4878314d63fb4fea94d6fabba1fb267809a09e41
Maxime Houis <maxime.houis@epitech.eu>
2020-04-16 14:15:13 +0200

    Implement Thread pools (#18)
    * First Thread pool draft
    
    * Fix Windows linking
    
    * Optimization: Create threads only when needed
    
    * Return main to its original state

---
+++ Engine/include/Engine/ThreadPool.hpp
@@ -0,0 +1,73 @@
+/*
+** RayTracer Copyright (C) 2020 Maxime Houis
+** This program comes with ABSOLUTELY NO WARRANTY.
+** This is free software, and you are welcome to redistribute it
+** under certain conditions; see LICENSE for details.
+*/
+
+#pragma once
+
+#include <queue>
+#include <vector>
+
+#include <mutex>
+#include <atomic>
+#include <thread>
+
+template<typename Ret, typename... Args>
+class ThreadPool {
+public:
+    static std::uint32_t MaxHardwareConcurrent;
+private:
+    std::function<Ret(Args...)> m_worker;
+    std::uint32_t m_maxConcurrent;
+
+    std::vector<std::thread> m_threads{};
+    std::queue<Args...> m_queue{};
+    std::atomic_uint32_t m_running{0};
+
+    void runTask(Args &&...args)
+    {
+        m_worker(args...);
+        --m_running;
+    }
+
+public:
+    explicit ThreadPool(std::function<Ret(Args...)> worker, std::uint32_t maxConcurrent = MaxHardwareConcurrent)
+            : m_worker{worker}, m_maxConcurrent{maxConcurrent}
+    {
+        if (maxConcurrent > MaxHardwareConcurrent) {
+            std::cerr << "WARNING: ThreadPool: " << maxConcurrent << '/' << MaxHardwareConcurrent << " threads scheduled\n";
+        } else {
+            std::cerr << "Creating a pool of " << m_maxConcurrent << " threads\n";
+        }
+    }
+
+    void queueTask(Args &&...args)
+    {
+        m_queue.push(std::forward<Args>(args)...);
+    }
+
+    void runAndWait()
+    {
+        while (true) {
+            if (m_queue.empty())
+                break;
+
+            while (m_running >= m_maxConcurrent) {
+                std::this_thread::yield();
+            }
+            ++m_running;
+
+            auto args = m_queue.front();
+            m_threads.emplace_back(&ThreadPool<Ret, Args...>::runTask, this, args);
+            m_queue.pop();
+        }
+
+        for (auto &thread : m_threads)
+            thread.join();
+    }
+};
+
+template<typename Ret, typename... Args>
+std::uint32_t ThreadPool<Ret, Args...>::MaxHardwareConcurrent = std::thread::hardware_concurrency();
--- RayTracer/src/Main.cpp
+++ RayTracer/src/Main.cpp
@@ -6,10 +6,29 @@
 */
 
 #include "RayTracerApp.hpp"
+#include "Engine/ThreadPool.hpp"
+
+//void do_work(int)
+//{
+//    using namespace std::chrono_literals;
+//
+//    static std::atomic<int> id = 0;
+//    int this_id = ++id;
+//
+//    std::cout << this_id << " starting" << std::endl;
+//    std::this_thread::sleep_for(1s);
+//    std::cout << this_id << " done" << std::endl;
+//}
 
 int main(int ac, char **av)
 {
     RayTracerApp app(ac, av);
 
     app.start();
+
+//    ThreadPool<void, int> tp(&do_work);
+//    for (int i = 0; i < 100; ++i) {
+//        tp.queueTask(1);
+//    }
+//    tp.runAndWait();
 }
