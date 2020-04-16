f320da7dd268e559af36c3f03eccc2b26371bcff
Pierre Langlois <pierrelgs.pro@hotmail.com>
2020-04-16 17:43:11 +0200

    [EDIT] Add sources file in the cmakeList

--- RayTracer/src/Main.cpp
+++ RayTracer/src/Main.cpp
@@ -1,65 +1,37 @@
 #include <iostream>
-#include <utility>
 #include <vector>
+
 #include "Engine/Window/Window.hpp"
 #include "Engine/Vector/Vector3.hpp"
 #include "Engine/Ray/Ray.hpp"
 #include "Objects/Sphere.hpp"
+#include "Objects/ObjectList.hpp"
 
-class hitable_list : public Object {
-public:
-    hitable_list()
-    = default;
-
-    explicit hitable_list(std::vector<Object *> obj) : list(std::move(obj))
-    {}
-
-    bool isHit(const raylib::Ray &r, float t_min, float t_max, raylib::RayHitInfo &rec) override
-    {
-        raylib::RayHitInfo temp_rec;
-        bool hit_anything = false;
-        float closest_so_far = t_max;
-        for (auto &i : list) {
-            if (i->isHit(r, t_min, closest_so_far, temp_rec)) {
-                // std::cout << list.size() << std::endl;
-
-                hit_anything = true;
-                closest_so_far = temp_rec.distance;
-                rec = temp_rec;
-            }
-        }
-        return hit_anything;
-    }
-
-    std::vector<Object *> list;
-};
-
-raymath::Vector3 linearInterpolation(const raylib::Ray &ray, Object *list)
+raymath::Vector3 linearInterpolation(raylib::Ray ray, Object *list)
 {
     raylib::RayHitInfo info;
-    if (list->isHit(ray, 0.0, MAXFLOAT, info)) {
+
+    //check if any ray hit an object 0 and MAXFLOAT are value to stop the calcul if no object is found or an object is too close
+    //When an obj is hit, RayHitInfo is Fill and the fct return True
+    if (list->isHit(ray, 0.0f, MAXFLOAT, info))
+    {
         raymath::Vector3 plop = 0.5f * raymath::Vector3(info.normal.x() + 1, info.normal.y() + 1, info.normal.z() + 1);
-        plop *= 255.0;
-        std::cout << (int) plop.x() << " " << (int) plop.y() << " " << (int) plop.z() << std::endl;
+        if (plop.z() > 1)
+            return raymath::Vector3(plop.x(), plop.y(), 1.0f);
         return plop;
-    } else {
+    }
+    else
+    {
         raymath::Vector3 vecteurUnitaire = normalize(ray.getDirection());
-        float t = 0.5f * (vecteurUnitaire.y() + 1.0f);
-        raymath::Vector3 plop = (1.0f - t) * raymath::Vector3(1.0, 1.0, 1.0) + t * raymath::Vector3(0.5, 0.7, 1.0);
-        plop *= 255.0;
-        std::cout << (int) plop.x() << " " << (int) plop.y() << " " << (int) plop.z() << std::endl;
-        // std::cout << 255 << " " << 255 << " " << 255 << std::endl;
-        return raymath::Vector3(255, 255, 255);
+        float t = 0.5 * (vecteurUnitaire.y() + 1.0);
+        return (1.0f - t) * raymath::Vector3(1.0, 1.0, 1.0) + t * raymath::Vector3(0.5, 0.7, 1.0);
     }
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
     int nx = 800;
     int ny = 600;
-
-    SetTraceLogLevel(LOG_NONE);
-    std::cout << "P3\n" << nx << ' ' << ny << "\n255\n";
 
     raylib::Window window(nx, ny, "Test");
 
@@ -72,26 +44,28 @@ int main()
 
     std::vector<Object *> obj;
     obj.push_back(new Sphere(raymath::Vector3(0, 0, -1), 0.5));
-    // obj.push_back(new Sphere(raymath::Vector3(0, 1.5, -1), 1));
-    Object *list = new hitable_list(obj);
-    while (window.isOpen()) {
+    Object *list = new ObjectList(obj);
+
+    while (window.isOpen())
+    {
         window.clear();
         BeginDrawing();
 
-        for (int j = ny - 1; j >= 0; j--) {
-            for (int i = 0; i < nx; i++) {
-                float Vu = (float) i / (float) (nx);
-                float Vv = (float) j / (float) (ny);
+        for (int j = ny - 1; j >= 0; j--)
+        {
+            for (int i = 0; i < nx; i++)
+            {
+                float Vu = (float)i / (float)(nx);
+                float Vv = (float)j / (float)(ny);
 
                 //Projection of the ray depending of the size of the screen
                 raylib::Ray ray(o, l + Vu * h + Vv * v);
 
                 raymath::Vector3 col = linearInterpolation(ray, list);
-                DrawPixel(i, j, Color{(uint8_t) (col.x()), (uint8_t) (col.y()), (uint8_t) (col.z()), 255});
+                DrawPixel(i, j, Color{(int)(col.x() * 255.99), (int)(col.y() * 255.99), (int)(col.z() * 255), 255});
             }
         }
         EndDrawing();
-        throw;
     }
     return 0;
 }
