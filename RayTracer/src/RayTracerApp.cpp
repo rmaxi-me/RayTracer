7fe5e82eb12c82d4e51e5550e6b34264b11c1c4b
Maxime Houis <maxime.houis@epitech.eu>
2020-04-21 13:43:12 +0200

    Fix black screen bug and improve Application (#37)
    * Add raygui dependency
    
    * Use better dependency installation
    
    * Fix black screen bug, improve Application class
    
    * Fix Windows build
    
    * Fix Windows build
    
    * Add first scene editor draft
    
    * Fix merge errors
    
    * Disable MacOS scene editor

--- RayTracer/src/RayTracerApp.cpp
+++ RayTracer/src/RayTracerApp.cpp
@@ -19,7 +19,7 @@
 #include "Scene/Scene.hpp"
 
 RayTracerApp::RayTracerApp(int ac, char **av)
-        : Application(ac, av)
+        : Application(ac, av, 800, 600)
 {
 }
 
@@ -42,11 +42,6 @@ void RayTracerApp::init()
 
 void RayTracerApp::deinit()
 {
-}
-
-void RayTracerApp::tick(float deltaTime)
-{
-    (void) deltaTime;
 }
 
 raymath::Vector3 colorize(const raylib::Ray &ray, const std::shared_ptr<ObjectList> &list)
@@ -104,7 +99,7 @@ void RayTracerApp::computePixelRange(std::vector<Pixel> &pixels, size_t begin, s
         computePixelColor(pixels[i]);
 }
 
-void RayTracerApp::draw()
+void RayTracerApp::tick(float)
 {
 //    m_window->clear();
 
@@ -124,12 +119,11 @@ void RayTracerApp::draw()
 
     for (auto &thread : threads)
         thread.join();
+}
 
-    BeginDrawing();
+void RayTracerApp::draw()
+{
     for (const auto &pixel : m_frameBuffer.pixels) {
         DrawPixel(pixel.x, m_frameBuffer.height - pixel.y, pixel.color.toColor());
     }
-    EndDrawing();
-
-    std::cout << "draw() frame time = " << GetFrameTime() << "\tfps = " << m_fps << '\n';
 }
